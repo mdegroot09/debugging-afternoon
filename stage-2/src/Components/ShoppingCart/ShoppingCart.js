efd6718c93f37a377bbd4b7980a2f98327d94e54
Mike De Groot <mdegroot09@gmail.com>
2019-04-09 16:20:34 -0600

    completed stage 2

--- stage-2/src/Components/ShoppingCart/ShoppingCart.js
+++ stage-2/src/Components/ShoppingCart/ShoppingCart.js
@@ -8,13 +8,13 @@ class ShoppingCart extends Component {
         <div className="shopping-cart-product-container" key={index}>
           <img src={element.image} alt="" />
           <div className="shopping-cart-info">
-            <h2>{element}</h2>
+            <h2>{element.title}</h2>
             <h2>{"$" + element.price + ".00"}</h2>
             <div className="shopping-cart-button-container">
               <button
                 className="shopping-cart-button"
-                onClick={() => this.props.removeFromCart(element)}
-              >
+                onClick={() => this.props.removeFromCart(index)}
+                >
                 Remove From Shopping Cart
               </button>
             </div>
@@ -27,10 +27,12 @@ class ShoppingCart extends Component {
         {shoppingCartDisplay[0] ? (
           shoppingCartDisplay
         ) : (
+         
           <div className="go-buy-something">
             <h1>Your shopping cart is empty! Go buy something!</h1>
           </div>
-        )}
+        )
+        }
       </div>
     );
   }
