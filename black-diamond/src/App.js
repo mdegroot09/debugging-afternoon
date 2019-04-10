1fe63cca7b423464335434387acb444a25511c72
Mike De Groot <mdegroot09@gmail.com>
2019-04-09 18:29:43 -0600

    completed black diamond

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -12,19 +12,26 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.addToCart = this.addToCart.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
+    this.navigate = this.navigate.bind(this)
   }
   componentDidMount() {
     axios
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response.data
+          products: response.data,
+          showCart: false
         });
       });
   }
   addToCart(item) {
+    let cart = [...this.state.cart]
+    cart.push(item)
     this.setState({
-      cart: [...this.state.cart, item]
+      cart: cart
     });
   }
   removeFromCart(index) {
@@ -36,9 +43,9 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true})
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false})
     }
   }
   render() {
@@ -49,9 +56,9 @@ class App extends Component {
         <div className="main-container">
           {showCart ? (
             <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
-          ) : (
+            ) : (
             <StoreFront products={products} addToCart={this.addToCart} />
-          )}
+            )}
         </div>
       </div>
     );
